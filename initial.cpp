e3dec054049a6369fbd966ba8cce97944a7776aa
Asheesh <Asheesh12345kumar@gmail.com>
2023-07-18 18:45:33 +0530

    18-july

---
+++ initial.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main(){
+    std::cout<<"hello world , My name is Asheesh";
+    return 0;
+}
+
---
+++ variables.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+// this is created by the Asheesh 
+/*
+Asheesh 
+is 
+here*/
+
+int global = 5;  
+// it is the global variable but have the less presendence then local variable 
+// means that if there exist the local variable then it will gonna print on the console 
+// global and the local can have the same name  
+
+int main(){
+    int sum=6;
+    float pie = 3.14;
+    char c= 'c';
+
+    int global = 45;
+
+    bool a= true;
+
+
+
+    cout << "Sum = \t"<<sum<<endl;
+    cout << "the value of the pie is "<< pie<<endl;
+    cout << "the value of the char is "<< c<<endl;
+    cout << "the value of the bool is "<< a<<endl;
+    cout << "the value of the global is "<< global<<endl;
+
+    return 0;
+}
