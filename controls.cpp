5e7ab8382f7564a1da7e60ee6a51368c87af6c3b
Asheesh <Asheesh12345kumar@gmail.com>
2023-07-20 18:45:24 +0530

    this is the video of the loops

---
+++ controls.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age ;
+    // cout<<"tell me your age " <<endl;
+    // cin>>age;
+    // if(age<18 && age >0 ){
+    //     cout<<"less";
+    // }
+    // else if(age==18){
+    //     cout<<"you are equal";
+
+    // }
+
+    // else {
+    //     cout<<"you are bigger";
+
+    // }
+
+
+    // switch (age)
+    // {
+    // case 12:
+    //     cout<<"big";
+        
+    //     break;
+    // case 18:
+    //     cout<<"big1";
+        
+    //     break;
+    // case 13:
+    //     cout<<"big2";
+        
+    //     break;
+    // case 15:
+    //     cout<<"big3";
+        
+    //     break;
+   
+    
+    // default:
+    //     break;
+    // } 
+
+
+
+// loops in c++
+
+        // for (int i=0;i<5;i++){
+        //     cout<<"fuck them"<<i<<endl;
+        // }
+
+           int i=0;
+        // while (i<14)
+        // {
+        //  cout<<"fuck them"<<i<<endl;
+
+        //     i++;
+        // }
+        
+
+        do{
+         cout<<"fuck them"<<i<<endl;
+            ++i;
+        } while (i<=40);
+         
+        
+
+
+    return 0;
+}
